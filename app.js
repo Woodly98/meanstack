1e83a10a2328a4427ab0e54c5ffd155686c676fa
Woodly <woodlysaintjour@gmail.com>
2017-09-06 20:20:14 -0400

    2ndcommit

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require ('express');
 const path = require ('path');
-const bodyparser= require ('body-parser');
+const bodyParser= require ('body-parser');
 const cors = require ('cors');
 const passport = require ('passport');
 const mongoose= require ('mongoose');
 
 const app =express();
 
+const users= require('./routes/users');
+
+//port number
 const port = 3000;
 
+// cors middleware
+app.use(cors());
+
+// Body parser middleware
+app.use(bodyParser.json());
+
+// index route
 app.get('/', (req, res)=> {
 res.send('invalid endpoint');
 });
+
+//start server
 app.listen(port, () => {
   console.log('server started on port '+port);
 });
